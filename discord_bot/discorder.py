ccc6dcbd384f5e984333b9c69e0e3d3991191ada
flashiam <Shivanshu Dubey>
2021-07-20 17:15:13 +0530

    discord functions

--- discord_bot/discorder.py
+++ discord_bot/discorder.py
@@ -1,20 +1,47 @@
 import discord
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
-
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == 'ping':
-            await message.channel.send('pong')
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 
-client = MyClient()
+@client.event
+async def on_ready():
+    print('logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-----')
 
-fmbot = client.run('ODY0MjIyMjY0NTI2MzcyODk0.YOyTtA.iG4kM1RX2siMdOnN9gu9crCfTiw')
+
+newUserDMMessage = "WELCOME!"
+
+
+# Public Welcome
+@client.event
+async def on_member_join(member):
+    print("Recognized that " + member.name + " joined")
+    await client.send_message(member, newUserDMMessage)
+    await client.send_message(discord.Object(id='CHANNELID'), 'Welcome!')
+    print("Sent message to " + member.name)
+    print("Sent message about " + member.name + " to #CHANNEL")
+
+
+# Mod Leave Announcement
+@client.event
+async def on_member_remove(member):
+    print("Recognized that " + member.name + " left")
+    await client.send_message(discord.Object(id='CHANNELID'), '**' + member.mention + '** just left.')
+    print("Sent message to #CHANNEL")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        await message.channel.send("Hey ! "+str(message.author)+" How are you doing today ?")
+
+
+client.run('ODY0MjIyMjY0NTI2MzcyODk0.YOyTtA.rJATI2gauORz56kNwjK4Gz-EtbY')
 
