e152625b4a1ec749772eaa985118a3911266c015
flashiam <shivanshu.dubey12@gmail.com>
2021-07-28 16:53:57 +0530

    Minor fixes

--- react-app/src/signin/SignInDiscord.tsx
+++ react-app/src/signin/SignInDiscord.tsx
@@ -4,18 +4,17 @@ import { connect } from "react-redux";
 import discordLogo from "../img/discord.svg";
 import { Button } from "react-bootstrap";
 
-
 const SignInDiscord = () => {
+  const verifyUrl =
+    "https://discord.com/api/oauth2/authorize?client_id=864222264526372894&permissions=0&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=bot%20guilds.join%20guilds%20gdm.join%20webhook.incoming%20activities.read%20activities.write%20identify";
 
-  const verifyUrl = "https://discord.com/api/oauth2/authorize?client_id=864222264526372894&permissions=0&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=bot%20guilds.join%20guilds%20gdm.join%20webhook.incoming%20activities.read%20activities.write%20identify"
- 
-// import { userSignIn } from "../actions/authActions";
+  // import { userSignIn } from "../actions/authActions";
 
-type Props = {
-  authenticateUser: Function;
-};
+  type Props = {
+    authenticateUser: Function;
+  };
 
-const SignInDiscord = ({ authenticateUser }: Props) => {
+  // const SignInDiscord = ({ authenticateUser }: Props) => {
   return (
     <main className="sign-in-discord bg-semi-med p-1">
       <div className="inner-content">
@@ -23,18 +22,17 @@ const SignInDiscord = ({ authenticateUser }: Props) => {
         <h3 className="head-3 primary">
           Connect your discord account to get the most out of Musix
         </h3>
-        <Button onClick={()=>{window.open(verifyUrl, 'newwindow', 'width=500,height=700')}} className="btn auth-btn discord-btn">
-          Connect to discord
-        </Button>
-        <button
+        <Button
+          onClick={() => {
+            window.open(verifyUrl, "newwindow", "width=500,height=700");
+          }}
           className="btn auth-btn discord-btn"
-          onClick={() => authenticateUser()}
         >
           Connect to discord
-        </button>
+        </Button>
       </div>
     </main>
   );
 };
-}
+// }
 export default SignInDiscord;
